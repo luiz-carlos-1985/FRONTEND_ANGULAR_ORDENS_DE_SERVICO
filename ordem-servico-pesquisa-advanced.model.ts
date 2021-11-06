b5c16deb91ebdf459dbf7eeb6ee9982246007d8e
Luiz Carlos <66796526+luiz-carlos-1985@users.noreply.github.com>
2021-11-05 23:15:56 -0300

    Update ordem-servico-pesquisa-advanced.model.ts

--- ordem-servico-pesquisa-advanced.model.ts
+++ ordem-servico-pesquisa-advanced.model.ts
@@ -1,8 +1,1 @@
-import { AbstractModel } from '../../core/models/abstract.model';
 
-export class OrdemServicoPesquisaAdvanced extends AbstractModel {
-
-    public tipoObjeto: string;
-    public marca: string;
-    public modelo: string;
-}
